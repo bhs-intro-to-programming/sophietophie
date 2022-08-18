cbc272223d9678f311891fd7aa8f6a3734be6ae5
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-08-18 15:03:22 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -41,6 +41,6 @@ drawCircle(width/2, height/2, 10, 'green');
 
 drawRect(100, 20, 60, 40, 'purple');
 
-drawFilledCircle(width - 50, height - 50, 50, 'red');
+drawFilledCircle(width - 150, height - 50, 50, 'red');
 
 drawFilledRect(5, height - 20, 45, 15, 'purple');
