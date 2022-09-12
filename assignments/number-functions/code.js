c53b7ac3ae8872a0c69014b8c25e75640520f8b5
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-09-12 15:29:37 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -24,7 +24,7 @@ const distance = (n1, n2) => {
   return Math.abs(n1 - n2);
 }
 const manhattanDistance = (x1, y1, x2, y2) => {
-  return Math.abs(x1 - x2) + (y1 - y2);
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
 const euclideanDistance = (x1, y1, x2, y2) => {
   return Math.sqrt((Math.abs(x1 - x2))**2 + (Math.abs(y1 - y2))**2);
