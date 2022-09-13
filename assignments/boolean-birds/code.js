1b1b17a6475dc88a5737999749c0ab8d14177ac7
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-09-13 15:10:22 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -26,6 +26,6 @@ const isBloggyBird = (isRed, isSpotted) => {
 const isFlibbleBird = (isRed, isSpotted) => {
   return !isRed&&isSpotted;
 }
-const isBloggyBird = (isRed, isSpotted) => {
-  return isRed&&!isSpotted;
+const isGlobbyBird = (isRed, isSpotted) => {
+  return !isRed&&!isSpotted;
 }
