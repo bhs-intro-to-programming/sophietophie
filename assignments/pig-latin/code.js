9f453d52f2f65c9751b8f4d95ed1187fd63c7d13
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-09-19 15:28:42 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -33,10 +33,14 @@
 // operator works.
 
 
-  const pigLatin = (s) => {
-    return s.substring(s.search(/[aeiou]/))+s.substring(0, s.search(/[aeiou]/))+'ay';
+const pigLatin = (s) => {
+  return s.substring(s.search(/[aeiou]/)) + s.substring(0, s.search(/[aeiou]/)) + 'ay';
+}
+const advancedPigLatin = (s) => {
+  if (s.search(/[aeiou]/) = 0) {
+    return 's' + 'way';
+  } else {
+    return s.substring(s.search(/[aeiou]/)) + s.substring(0, s.search(/[aeiou]/)) + 'ay';
   }
-  const advancedPigLatin = (s) => {
-    if (s=aeiou) {
-    console.log(s.length-1)+'way';
-  }}
+
+}
