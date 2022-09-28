0ab3e05f8f499811091d03c8562ef5a0fb28a68d
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-09-28 14:46:29 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -24,16 +24,10 @@
  *  clear()
  */
 const drawLineCircle = (r) => {
-  for (let e = 0; e < 10000; e++) {
-    drawFilledCircle(e*r*2, 180, r, 'red');
+  for (let i = 0; i < 10000; i++) {
+    drawFilledCircle(i*r*2, 180, r, 'red');
 
   }
 }
 
 drawLineCircle(12)
-
-const drawLineCircle = (r) => {
-  for (let i = 0; i < 10000; i++) {
-    drawFill
-  }
-}
