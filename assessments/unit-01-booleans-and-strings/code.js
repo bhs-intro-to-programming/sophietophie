37afcd9dcaccab55daa6c8719c6390075a2c8221
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-10-26 15:00:35 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -28,3 +28,7 @@ const isLeapYear = (year) => {
 const firstAndLast = (s) => {
   return s[0]+s[s.length-1];
 }
+const swapFrontAndBack = (s) => {
+  return s.substring(s.length/2) + s.substring(0, s.length/2);
+}
+}
