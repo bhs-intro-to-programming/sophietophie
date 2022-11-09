87746b6ff6d8c47c5fb35744023d8760856cd5b8
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-11-09 14:52:14 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -2,15 +2,18 @@ const getX = (obj) => {
   return obj.x;
 }
 const point = (num1, num2) => {
-  return {x: num1, y: num2}
+  return { x: num1, y: num2 }
 }
 const emptyObject = () => {
   const obj = {}
   return obj
 }
 const distance = (p1, p2) => {
-  return Math.sqrt((Math.abs(p1.x - p2.x)**2) + (Math.abs(p1.y - p2.y)**2))
+  return Math.sqrt((Math.abs(p1.x - p2.x) ** 2) + (Math.abs(p1.y - p2.y) ** 2))
 }
 const midpoint = (p1, p2) => {
-  return {x: 0, y: 0}
+  return {
+    x: Math.sqrt((Math.abs(p1.x + p2.x) / 2), + (Math.abs(p1.y + p2.y)
+    y: 0
+  }
 }
