e69750dd5ad0204a433c4558e6b6bce8528d0867
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-11-10 15:02:23 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -27,7 +27,7 @@ return total
 const newHighScore = (currentHigh, objAr) => {
   let high = currentHigh
   for (let i = currentHigh; i < objAr.length; i++) {
-    high = high + objAr[i]
+    high = high + objAr[i].score
   }
   return high
 }
