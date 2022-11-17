eb58adb0217eaa66163cdc8f9e1d80246a11790b
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-11-17 15:17:17 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -8,5 +8,5 @@ const firstAndLast = (s) => {
   return s[0] + s[s.length - 1]
 }
 const swapFrontAndBack = (s) => {
-  return s.substring(s.length/2) 
+  return s.substring(s.length/2) + s.substring(0, s.length/2)
 }
