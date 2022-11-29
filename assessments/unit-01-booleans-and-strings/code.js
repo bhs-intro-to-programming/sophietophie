02405d095d57f57d152a3d60e0af79be4cfc8545
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-11-29 15:23:24 -0800

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -9,31 +9,3 @@ const rand = (n) => Math.floor(Math.random() * n);
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
-
-const fireAlarm = (alarmPulled, smokeDetected, plannedDrill) => {
-return alarmPulled || smokeDetected || plannedDrill;
-}
-const canBePresident = (age, naturalBorn, lived) => {
-  return age >= 35 && naturalBorn, lived >= 14;
-}
-const willSeeTweet = (follow, followRetweet, blocked) => {
-  return follow || followRetweet || !blocked;
-}
-const evenGreaterThanZero = (x) => {
-  return x>0
-}
-const isLeapYear = (year) => {
-  return even4(!even100||even400)
-}
-const firstAndLast = (s) => {
-  return s[0]+s[s.length-1];
-}
-const swapFrontAndBack = (s) => {
-  return s.substring(s.length/2) + s.substring(0, s.length/2);
-}
-const simplePigLatin = (s, b) => {
-  return s.substring(b) + s.substring(0, s.length/2) + 'ay';
-}
-const swapFrontAndBack = (s) => {
-  return s.substring(s.length/2) + s.substring(0, s.length/2);
-  }
