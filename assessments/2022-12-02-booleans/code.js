761a2bed50aab573f46efa8d8f2a5014621c7a0c
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-02 15:10:01 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -8,5 +8,14 @@ const needMittens = (outside, warm) => {
   return outside && !warm;
 }
 const isVenomous = (striped, blueHead) => {
-  return striped || !striped || !blueHead
+  return striped || !striped || !blueHead;
+}
+const okaySpeed = (speedLimit, carSpeed) => {
+  return carspeed <= speedLimit + 10 && carSpeed >= speedLimit -10;
+}
+const twiceAsExpensive = (item1, item2) => {
+  return item1 > item2 * 2;
+}
+const winningRecord = (wins, losses, ties) => {
+  return wins > losses && ties
 }
