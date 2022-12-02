2e9c9a1ed6a718b4fccd034b5e42f2aac455c0f2
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-02 14:46:57 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,3 +1,12 @@
 const needHeavyCoat = (outside, cold) => {
-  return outside || cold
+  return outside && cold;
+}
+const needSunscreen = (beach, ski) => {
+  return beach && ski;
+}
+const needMittens = (outside, warm) => {
+  return outside && !warm;
+}
+const isVenomous = (striped, blueHead) => {
+  return striped || blueHead 
 }
