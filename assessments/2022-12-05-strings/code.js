333bb0b904298f5502965a22c576cfe940814e6e
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-05 15:13:57 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -1,24 +1,24 @@
 const upToX = (s) => {
-  return s.substring(s.length - 'x')
+  return s.substring(s.length - 'x');
 }
-const charactersAround = (s, index) => {
-  return 
+const charactersAround = (s, i) => {
+  return s.substring(i - 1) + s.substring(i + 1);
 }
 const middle = (s) => {
-  return s.substring(0, s.length/2) + s.substring(s.length/2)
+  return s.substring(0, s.length/2) + s.substring(s.length/2);
 }
 const pair = (s1, s2) => {
-  return s1 + 'and' + s2
+  return s1 + 'and' + s2;
 }
 containsX = (s) => {
-  return s.substring('x') 
+  return s.substring('x');
 }
 const slug = (s1, s2, s3) => {
-  return s1 + '-' + s2 + '-' + s3
+  return s1 + '-' + s2 + '-' + s3;
 }
 const capitalize = (s) => {
-  return s.toUpperCase(0)
+  return s.toUpperCase(0);
 }
 const capitalizeName = (s) => {
-  return s.toUpperCase(0) + s.toUpperCase(s.length/2)
+  return s.toUpperCase(0) + s.toUpperCase(s.length/2);
 }
