1e471de29465843c92e4483a6ee6d04dfb4f82cd
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-05 15:00:28 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -1,3 +1,9 @@
 const upToX = (s) => {
   return s.substring(s.length - x)
 }
+const charactersAround = (s, index) => {
+  return 
+}
+const middle = (s) => {
+  return s.substring(0, s.length/2) + s.substring(s.length/2)
+}
