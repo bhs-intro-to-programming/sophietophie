ebd419a64e27fed2eeec71dea358aa9c4dbdef61
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-06 10:20:51 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -11,7 +11,7 @@ const middle = (s) => {
 const pair = (s1, s2) => {
   return s1 + (' ') + ('and') + (' ') + s2;
 }
-containsX = (s) => {
+const containsX = (s) => {
   return s.substring('x');
 }
 const slug = (s1, s2, s3) => {
