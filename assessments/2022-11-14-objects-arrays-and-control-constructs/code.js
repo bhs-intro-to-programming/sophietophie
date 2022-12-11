430166384746e0d0d21b9a758facaca525406f65
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-11 11:02:24 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -4,3 +4,24 @@
 const area = (rect) => {
   return rect.width * rect.height;
 };
+const higherPaid = (employee1, employee2) => {
+  if (employee1.salary > employee2.salary) {
+    return employee1;
+  } else {
+    return employee2;
+  }
+};
+const isSamePoint = (p1, p2) => {
+  return (p1.x === p2.x) && (p1.y === p2.y);
+};
+const totalWithTip = (bill, percentageTip) => {
+  return {
+    subtotal: bill.subtotal, 
+    tip: bill.subtotal * percentageTip,
+    total: bill.subtotal + (bill.subtotal * percentageTip),
+  
+  }
+}
+const isWinner = (obj) => {
+  return obj.score > 100;
+};
