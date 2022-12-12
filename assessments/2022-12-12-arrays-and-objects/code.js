b2376af696e012e35eabc3fa5766994ffee556d0
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-12 15:09:12 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

---
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -0,0 +1,12 @@
+const makeRow = () => {
+  return [ , , ];
+};
+const makeBoard = () => {
+  
+  }
+}
+const makeMove = (strM, numR, numC) => {
+  const strM: mark;
+  const numR: row;
+  const numC: column;
+};
