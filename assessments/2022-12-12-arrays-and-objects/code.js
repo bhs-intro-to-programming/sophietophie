8ac159db9ee80b5b9f599eaf66d78e96a02abece
sophietophie <111394717+sophietophie@users.noreply.github.com>
2022-12-12 15:16:32 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -9,3 +9,11 @@ const makeMove = (strM, numR, numC) => {
   const numR: row;
   const numC: column;
 };
+const allTheSame = (elements) => {
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i] === elements[i]) {
+      return true
+    } else {
+      return false
+    };
+  };
